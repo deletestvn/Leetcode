b0605a450ac8239729b880da3a1c2abcec34a77b
deletestvn <deletestvn@gmail.com>
2020-07-22 00:07:19 -0400

    Update LinkedList Conclusion solutions

--- src/Learn/LinkedList/copyListWithRandomPointer.cpp
+++ src/Learn/LinkedList/copyListWithRandomPointer.cpp
@@ -17,6 +17,32 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        
+        if(head == NULL) return NULL;
+
+        Node * copy, * currCopy, * currPaste;
+
+        // Step 1: create inter weaved list 
+        for(currCopy = head; currCopy != NULL; currCopy = currCopy -> next -> next) {
+            // Old List: A --> B --> C --> D
+            // Inter Weaved List: A --> A' --> B --> B' --> C --> C' --> D --> D'
+            currPaste = new Node(currCopy -> val);
+            currPaste -> next = currCopy -> next;
+            currCopy -> next = currPaste;
+        }
+
+        // Step 2: get correct reference nodes for random pointers
+        for(currCopy = head; currCopy != NULL; currCopy = currCopy -> next -> next) {
+            if(currCopy -> random != NULL) currCopy -> next -> random = currCopy -> random -> next;
+        }
+        copy = head -> next;
+
+        // Step 3: remove original nodes
+        for(currCopy = head; currCopy != NULL; currCopy = currCopy -> next) {
+            currPaste = currCopy -> next;
+            currCopy -> next = currPaste -> next;
+            if(currPaste -> next != NULL) currPaste -> next = currPaste -> next -> next;
+        }
+
+        return copy;
     }
 };
